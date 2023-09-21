8c232da2691dca0cf8f5f249835229886b813807
aninha029 <137413193+aninha029@users.noreply.github.com>
2023-09-21 10:30:24 -0300

    mesadodj

---
+++ main.js
@@ -0,0 +1,11 @@
+function toca som (idElementoAudio)  {
+    document.querySelector(idElementoAudio).play();
+    {
+        const listaDeTeclas = document.querySelectorA11('.tecla');
+    //para
+    for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+        const tecla = listaDeTeclas[contador]; 
+        const instrumento - tecla.classList[1];
+        const idAudio - #som_${instrumento}´; //template string
+        tecla.onclick = function ()  {
+            tocaSom(idAudio);
