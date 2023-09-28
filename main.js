382218b6b186e81f813b134eccd0a5830305a9a1
aninha029 <137413193+aninha029@users.noreply.github.com>
2023-09-28 10:26:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-function toca som (idElementoAudio)  {
+function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
-    {
-        const listaDeTeclas = document.querySelectorA11('.tecla');
-    //para
-    for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-        const tecla = listaDeTeclas[contador]; 
-        const instrumento - tecla.classList[1];
-        const idAudio - #som_${instrumento}´; //template string
-        tecla.onclick = function ()  {
-            tocaSom(idAudio);
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
